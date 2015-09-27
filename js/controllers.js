5a3ca9219987c916892f76b2d3221f4eb80e6b98
ericquayson <ecbaiden@ncsu.edu>
2015-09-27 17:17:00 -0400

    refactored the code

--- js/controllers.js
+++ js/controllers.js
@@ -7,40 +7,53 @@
    * 3. What `CALLBACK` should be run when the interaction happens?
    * 4. What should my `CALLBACK` do with it's `EVENT`...?
    */
-  document.querySelector(SELECTOR)
-    .addEventListener(TYPE, CALLBACK);
+  document.querySelector("caption")
+    .addEventListener('click', 'button', function () {
+      prompt("hello")
+    });
   // AKA
-  jQuery(SELECTOR).on(TYPE, CALLBACK);
+  $("caption").on('click', 'button' function () {
+
+
+    
+  });
   // Where CALLBACK is...
   function CALLBACK (EVENT){
     // Do something with Models and Views...
     // Maybe do something with EVENT...?
+    event.;
   }
 
 
   // Controller for "next move"...
-  jQuery(/* your "next move" button */).on('click', function(event){
+  // /* your "next move" button */
+  $(".fafa-play" ).on('click', function(e){
     // TODO: Fire tracer bullet!
+    alert("hello")
     // TODO: Tell the Model -- `game` -- to advance to the next move...
+    $()
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
   });
 
   // Controller for "previous move"...
-  jQuery(/* your "previous move" button */).on('click', function(event){
+  // /* your "previous move" button */
+  $(".fafa-step-backward").on('click', 'button', function(e){
     // TODO: Fire tracer bullet!
     // TODO: Tell the Model -- `game` -- to advance to the previous move...
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
   });
 
   // Controller for "fast-forward"...
-  jQuery(/* your "fast-forward" button */).on(/* wat? */, function(event){
+  // /* your "fast-forward" button */
+  $("fa fa-fast-forward").on('click', 'button', function(e){
     // TODO: Fire tracer bullet!
     // TODO: Tell the Model -- `game` -- to advance to the last move...
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
   });
 
   // Controller for anything else...
-  jQuery(/* any other buttons out there? */).on(/* dunno */, function(event){
+  // /* any other buttons out there? */
+  $(".fafa-fast-backward").on('click', 'button' function(e){
     // TODO: Fire tracer bullet!
     // TODO: Tell the Model -- `game` -- to do something it knows how to do...
     // TODO: Tell the View -- `.chessboard` -- to update the position of the pieces based on `game.board()`
